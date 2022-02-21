079235bb07a5ec5cb626fce12253458631a260bf
Henrique belard pignaton <38145467+henrique-pignaton@users.noreply.github.com>
2022-02-21 14:30:36 -0300

    certo

---
+++ script.js
@@ -0,0 +1,46 @@
+var data = new Date()
+var dia = data.getDate()
+console.log(dia)
+var div = document.getElementById("msg-motivacional")
+ var dia =1
+
+const msg1 = "Eu tentei 99 vezes e falhei, mas na centésima tentativa eu consegui, nunca desista de seus objetivos mesmo que esses pareçam impossíveis, a próxima tentativa pode ser a vitoriosa. <i> <strong>Albert Einstein</strong></i>"
+const msg2 = ""
+const msg3 = ""
+const msg4 = ""
+const msg5 = ""
+const msg6 = ""
+const msg7 = ""
+
+
+if (dia == 1) {
+    div.innerHTML = msg1
+}
+if (dia == 2) {
+    div.innerHTML = msg2
+}
+if (dia == 3) {
+    div.innerHTML = msg3
+}
+if (dia == 4) {
+    div.innerHTML = msg4
+}
+if (dia == 5) {
+    div.innerHTML = msg5
+}
+if (dia == 6) {
+    div.innerHTML = msg6
+}
+if (dia == 7) {
+    div.innerHTML = msg7
+}
+if (dia == 8) {
+    div.innerHTML = 'Teste dia 21'
+}
+if (dia == 9) {
+    div.innerHTML = 'Teste dia 21'
+}
+if (dia == 10) {
+    div.innerHTML = 'Teste dia 21'
+}
+
