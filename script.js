30c954dc37ceedfc0d34ba41dd800ff707190573
Henrique belard pignaton <38145467+henrique-pignaton@users.noreply.github.com>
2022-02-21 15:27:26 -0300

    finalização

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var data = new Date()
 var dia = data.getDate()
 var div = document.getElementById("msg-motivacional")
 
-
+//Frases
 const msg1 = "Eu tentei 99 vezes e falhei, mas na centésima tentativa eu consegui, nunca desista de seus objetivos mesmo que esses pareçam impossíveis, a próxima tentativa pode ser a vitoriosa. <i> <strong>Albert Einstein</strong></i>"
 
 const msg2 = "Nas grandes batalhas da vida, o primeiro passo para a vitória é o desejo de vencer.<i> <strong>Gandhi</strong></i>"
@@ -51,30 +51,21 @@ const msg23 = '<br>O tempo é muito lento para os que esperam <br> Muito rápido
 
 const msg24 = 'A maior covardia de um homem é despertar o amor de uma mulher sem ter a intenção de amá-la. <i><strong>Augusto Branco</strong></i>'
 
-const msg25= 'Sim, minha força está na solidão. Não tenho medo nem de chuvas tempestivas nem das grandes ventanias soltas, pois eu também sou o escuro da noite <i><strong>Clarice Lispector</strong></i>'
+const msg25 = 'Sim, minha força está na solidão. Não tenho medo nem de chuvas tempestivas nem das grandes ventanias soltas, pois eu também sou o escuro da noite <i><strong>Clarice Lispector</strong></i>'
 
 const msg26 = 'Só existem dois dias no ano que nada pode ser feito. Um se chama ontem e o outro se chama amanhã, portanto hoje é o dia certo para amar, acreditar, fazer e principalmente viver.    <i><strong>Dalai Lama</strong></i>'
 
-const msg27= 'Deus, para a felicidade do homem, inventou a fé e o amor. O Diabo, invejoso, fez o homem confundir fé com religião e amor com casamento. <i><strong>Machado de Assis</strong></i>'
+const msg27 = 'Deus, para a felicidade do homem, inventou a fé e o amor. O Diabo, invejoso, fez o homem confundir fé com religião e amor com casamento. <i><strong>Machado de Assis</strong></i>'
 
 const msg28 = 'Se tu me amas, ama-me baixinho <br>Não o grites de cima dos telhados <br>Deixa em paz os passarinhos <br>Deixa em paz a mim! <br>Se me queres, <br>enfim, <br>tem de ser bem devagarinho, Amada, <br>que a vida é breve, e o amor mais breve ainda... <i><strong>Mario Quintana</strong></i>'
 
-const msg29 = ''
+const msg29 = 'Saudade é um pouco como fome. Só passa quando se come a presença. Mas às vezes a saudade é tão profunda que a presença é pouco: quer-se absorver a outra pessoa toda. Essa vontade de um ser o outro para uma unificação inteira é um dos sentimentos mais urgentes que se tem na vida.<i><strong>Clarice Lisnpector</strong></i>'
 
-const msg30 = ''
+const msg30 = 'Faça o que for necessário para ser feliz. Mas não se esqueça que a felicidade é um sentimento simples, você pode encontrá-la e deixá-la ir embora por não perceber sua simplicidade. <i><strong>Martha Medeiros</strong></i>'
 
-const msg31 = ''
+const msg31 = 'O mundo é grande e cabe nesta janela sobre o mar. O mar é grande e cabe na cama e no colchão de amar. O amor é grande e cabe no breve espaço de beijar. <i><strong>Carlos Drummond de Andrade</strong></i>'
 
-
-
-
-
-
-
-
-
-
-
+//condiçoes
 if (dia == 1) {
     div.innerHTML = msg1
 }
@@ -141,8 +132,32 @@ if (dia == 21) {
 if (dia == 22) {
     div.innerHTML = msg22
 }
+if (dia == 23) {
+    div.innerHTML = msg23
+}
+if (dia == 24) {
+    div.innerHTML = msg24
+}
+if (dia == 25) {
+    div.innerHTML = msg25
+}
+if (dia == 26) {
+    div.innerHTML = msg26
+}
+if (dia == 27) {
+    div.innerHTML = msg27
+}
 if (dia == 28) {
     div.innerHTML = msg28
+}
+if (dia == 29) {
+    div.innerHTML = msg29
+}
+if (dia == 30) {
+    div.innerHTML = msg30
+}
+if (dia == 31) {
+    div.innerHTML = msg31
 }
 
 
