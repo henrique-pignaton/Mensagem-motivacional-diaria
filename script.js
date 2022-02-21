1ff03ff618af45275933d7b6b7e3006012c8b1e8
Henrique belard pignaton <38145467+henrique-pignaton@users.noreply.github.com>
2022-02-21 14:41:54 -0300

    adição dos dias

--- script.js
+++ script.js
@@ -2,15 +2,25 @@ var data = new Date()
 var dia = data.getDate()
 console.log(dia)
 var div = document.getElementById("msg-motivacional")
- var dia =1
+var dia = 1
 
 const msg1 = "Eu tentei 99 vezes e falhei, mas na centésima tentativa eu consegui, nunca desista de seus objetivos mesmo que esses pareçam impossíveis, a próxima tentativa pode ser a vitoriosa. <i> <strong>Albert Einstein</strong></i>"
-const msg2 = ""
-const msg3 = ""
-const msg4 = ""
-const msg5 = ""
-const msg6 = ""
-const msg7 = ""
+
+const msg2 = "Nas grandes batalhas da vida, o primeiro passo para a vitória é o desejo de vencer.<i> <strong>Gandhi</strong></i>"
+
+const msg3 = "Quando era jovem, descobri que nove de cada dez coisas que eu fazia eram um fracasso. Eu não queria ser um fracasso. Então, passei a trabalhar dez vezes mais. <i><strong>George Bernard Shaw</strong></i>"
+
+const msg4 = "É fazendo que se aprende a fazer aquilo que se deve aprender a fazer. <i><strong>Aristóteles</strong></i>"
+
+const msg5 = "Uma chave importante para o sucesso é a auto-confiança. Uma chave importante para a auto-confiança é a preparação. <i><strong>Arthur Ashe</strong></i>"
+
+const msg6 = "O sucesso é a soma de pequenos esforços repetidos dia após dia <i><strong>Autor desconhecido</strong></i>"
+
+const msg7 = "A felicidade é uma borboleta que, sempre que perseguida, parecerá inatingível; no entanto, se você for paciente, ela pode pousar no seu ombro. <i><strong>Nathaniel Hawthorne</strong></i>"
+
+const msg8 = " Comece de onde você está. Use o que você tiver. Faça o que você puder. <i><strong>Arthur Ashe</strong></i>"
+
+const msg9 = 'Pessoas de sucesso fazem o que pessoas malsucedidas não querem fazer. Não queira que a vida seja mais fácil. Deseje que você seja ainda melhor. <i><strong>Jim Rohn<w/strong></i>'
 
 
 if (dia == 1) {
